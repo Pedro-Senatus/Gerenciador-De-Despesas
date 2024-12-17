d96a40b828f886b49db990cb56f9c22cf5b370d8
Pedro-Senatus <pedrofigo123@gmail.com>
2024-12-16 22:56:17 -0300

    update api

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { Expenses } from "../types/Expenses";
 
-const API_URL =  process.env.REACT_APP_API_URL;
+const API_URL =  process.env.REACT_APP_API_URL!;
 
 export const fetchData = async () =>{
 
