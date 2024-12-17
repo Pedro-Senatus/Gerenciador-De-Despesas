85e1ea4edd07fb3a26c8c32893fccb5ac1f80f5c
Pedro Santos <127903586+Pedro-Senatus@users.noreply.github.com>
2024-12-16 21:31:09 -0300

    Update api.ts

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { Expenses } from "../types/Expenses";
 
-const API_URL = 'http://localhost:3000/expenses';
+const API_URL = 'process.env.REACT_APP_API_URL';
 
 export const fetchData = async () =>{
 
